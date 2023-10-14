4c68284db6a02eb2460f75a11597c256fab11483
24degrees <24@24degrees.be>
2023-10-14 12:07:41 +0200

    Display the values of the metaboxes.

--- views/mv-slider_metabox.php
+++ views/mv-slider_metabox.php
@@ -1,3 +1,8 @@
+<?php
+//$meta = get_post_meta( $post->ID, 'mv_slider_link_text', true );
+$link_text = get_post_meta($post->ID, 'mv_slider_link_text', true);
+$link_url = get_post_meta($post->ID, 'mv_slider_link_url', true);
+?>
 <table class="form-table mv-slider-metabox">
     <tr>
         <th>
@@ -5,17 +10,16 @@
             <!-- note that the for attribute matches the name of the input -->
         </th>
         <td>
-            <input type="text" name="mv_slider_link_text" id="mv_slider_link_text" class="regular-text link-text" value="" required>
+            <input type="text" name="mv_slider_link_text" id="mv_slider_link_text" class="regular-text link-text" value="<?php echo $link_text; ?>" required>
         </td>
     </tr>
     <tr>
         <th>
             <label for="mv_slider_link_url">Link URL</label>
             <!-- note that the for attribute matches the name of the input -->
-
         </th>
         <td>
-            <input type="url" name="mv_slider_link_url" id="mv_slider_link_url" class="regular-text link-url" value="" required>
+            <input type="url" name="mv_slider_link_url" id="mv_slider_link_url" class="regular-text link-url" value="<?php echo $link_url; ?>" required>
         </td>
     </tr>
 </table>
