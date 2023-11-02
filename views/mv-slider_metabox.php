06c17b26eb7cd025bd83845dd7e2b533fc2c019b
24degrees <24@24degrees.be>
2023-11-02 10:16:32 +0100

    Back on it

--- views/mv-slider_metabox.php
+++ views/mv-slider_metabox.php
@@ -4,13 +4,15 @@ $link_text = get_post_meta($post->ID, 'mv_slider_link_text', true);
 $link_url = get_post_meta($post->ID, 'mv_slider_link_url', true);
 ?>
 <table class="form-table mv-slider-metabox">
+    <input type="hidden" name="mv_slider_nonce" value="<?php echo wp_create_nonce( 'mv_slider_nonce'); ?>">
     <tr>
         <th>
             <label for="mv_slider_link_text">Link Text</label>
             <!-- note that the for attribute matches the name of the input -->
         </th>
         <td>
-            <input type="text" name="mv_slider_link_text" id="mv_slider_link_text" class="regular-text link-text" value="<?php echo $link_text; ?>" required>
+            <input type="text" name="mv_slider_link_text" id="mv_slider_link_text" class="regular-text link-text" 
+            value="<?php echo ( isset( $link_text ) ? esc_html($link_text) : '' ) ?>" required>
         </td>
     </tr>
     <tr>
@@ -19,7 +21,8 @@ $link_url = get_post_meta($post->ID, 'mv_slider_link_url', true);
             <!-- note that the for attribute matches the name of the input -->
         </th>
         <td>
-            <input type="url" name="mv_slider_link_url" id="mv_slider_link_url" class="regular-text link-url" value="<?php echo $link_url; ?>" required>
+            <input type="url" name="mv_slider_link_url" id="mv_slider_link_url" class="regular-text link-url" 
+            value="<?php echo isset( $link_url ) ? esc_url($link_url) : '' ?>" required>
         </td>
     </tr>
 </table>
